8df5ec34b2b9bd70beaf8e2f85b456f3f39a2b8e
programwithus <artyudin@programwithus.com>
2021-07-13 16:45:10 -0400

    ll

--- app/hello_django/urls.py
+++ app/hello_django/urls.py
@@ -14,5 +14,5 @@ urlpatterns = [
     path("admin/", admin.site.urls),
 ]
 
-if bool(settings.DEBUG):
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
