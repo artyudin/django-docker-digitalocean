e89c1c2042f3439c02275bda934ba1c51eac43dc
programwithus <artyudin@programwithus.com>
2021-07-13 16:38:56 -0400

    ll

--- app/hello_django/settings.py
+++ app/hello_django/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
-DEBUG = int(os.environ.get("DEBUG", default=0))
+DEBUG = False
 
 ALLOWED_HOSTS = ["*", "127.0.0.1"]
 
